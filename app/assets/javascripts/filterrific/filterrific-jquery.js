d5c1f17798480c5a233af133589128e65961de27
hemmingbjarni <98856931+hemmingbjarni@users.noreply.github.com>
2023-12-10 03:09:30 +0400

    Update filterrific-jquery.js

--- app/assets/javascripts/filterrific/filterrific-jquery.js
+++ app/assets/javascripts/filterrific/filterrific-jquery.js
@@ -20,77 +20,51 @@ function throttle(f, delay){
     };
 }
 
-// Create global Filterrific namespace
 if (typeof Filterrific === 'undefined') {
   var Filterrific = {};
 }
 
-
-
-// Define function to submit Filterrific filter form
 Filterrific.submitFilterForm = function(event){
-  console.log("Inside submitFilterForm function");
 
   var form = $(event.target).closest("form"),
       url = form.attr("action");
 
-  console.log("Form action URL:", url);
-  console.log("Form data:", form.serialize());
-
-  // send before event
   $(form).trigger('loadingFilterrificResults');
-  // turn on spinner
   $('.filterrific_spinner').show();
 
-  // Abort previous ajax request
   if (Filterrific.lastRequest && Filterrific.lastRequest.readyState != 4) {
-    console.log("Aborting previous AJAX request");
     Filterrific.lastRequest.abort();
   }
 
-  // Submit ajax request
   Filterrific.lastRequest = $.ajax({
     url: url,
     data: form.serialize(),
     type: 'GET',
     dataType: 'script'
   }).done(function( msg ) {
-    console.log("AJAX request completed");
-    // send after event
     $(form).trigger('loadedFilterrificResults');
     $('.filterrific_spinner').hide();
   }).fail(function(jqXHR, textStatus) {
-    console.log("AJAX request failed:", textStatus);
   });
 
-  console.log("AJAX request sent");
 };
 
-
-
-//
-// Embed jquery.observe_field.js to observe Filterrific filter inputs
-//
-// Copied from https://github.com/splendeo/jquery.observe_field
-// Wrap in immediately invoked function for compatibility with other js libraries
-//
 (function($) {
 
   $.fn.filterrific_observe_field = function(frequency, callback) {
-    frequency = frequency * 1000; // translate to milliseconds
+    frequency = frequency * 1000;
     return this.each(function(){
       var $this = $(this);
       var prev = $this.val();
       var check = function() {
-        if(removed()){ // if removed clear the interval and don't fire the callback
+        if(removed()){
           if(ti) clearInterval(ti);
           return;
         }
         var val = $this.val();
         if(prev != val){
-        console.log("Observed field change");
           prev = val;
-          $this.map(callback); // invokes the callback on $this
+          $this.map(callback);
         }
       };
       var removed = function() {
@@ -103,20 +77,18 @@ Filterrific.submitFilterForm = function(event){
         }
       };
       check();
-      var ti = setInterval(check, frequency); // invoke check periodically
-      // reset counter after user interaction
-      $this.bind('keyup click mousemove', reset); //mousemove is for selects
+      var ti = setInterval(check, frequency);
+      $this.bind('keyup click mousemove', reset);
     });
   };
 })(jQuery);
 
 
 Filterrific.init = function() {
-  var lastVal = ''; // Variable to store the last value
+  var lastVal = '';
 
 $('#filterrific_filter').on("keyup", ":input", throttle(function(event) {
   lastVal = $(this).val(); 
-  console.log("Form submission triggered by keyup. Value:", lastVal);
   Filterrific.submitFilterForm(event);
 }, 500));
 
@@ -124,17 +96,12 @@ $('#filterrific_filter').on("change", ":input", function(event) {
   var currentVal = $(this).val();
   if (currentVal !== lastVal) {
     lastVal = currentVal;
-    console.log("Form submission triggered by change. New Value:", currentVal);
     Filterrific.submitFilterForm(event);
   }
 });
 };
 
-
-// Initialize event observers on document ready and turbolinks page:load
 jQuery(document).on('turbolinks:load', function() {
-  // Prevent double initilisation. With turbolinks 5 this function
-  // will be called twice: on 'ready' and 'turbolinks:load'
   jQuery(document).off('ready page:load')
   Filterrific.init();
 });
